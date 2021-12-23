679eb3e9da2b985e7759e5928cd885301a0a8819
Coeat.k <justkukaro@naver.com>
2021-12-23 09:13:00 +0900

    feat: edit writing file logic

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,9 +7,12 @@ base_url = 'https://ep.knou.ac.kr/portal/epo/service/retrieveIntgBdotDetail.do?b
 base_no = 2050543
 # jsessionid는 로그인하고 나서 확인하고 쓰세요
 jsessionid = '03C06990C5BBFCD283480A4854676401.74f67ab825f506361'
+time = datetime.datetime.now()
+now = '%04d%02d%02d%02d%02d%02d' % (time.year, time.month, time.day, time.hour, time.minute, time.second)
+f = open('%s.result.txt' % now, 'w')
 
 
-def f(base_url, base_no, jsessionid, debug=False):
+def read(base_url, base_no, jsessionid, debug=False, isWrite=False):
     html = requests.get(base_url + str(base_no), cookies={
         'JSESSIONID': jsessionid
     }).text
@@ -15,24 +20,32 @@ def f(base_url, base_no, jsessionid, debug=False):
     td_list = bs.find('tbody').find_all('td')
     title = td_list[0].text
     writer = td_list[1].text
+    date = td_list[2].text
     files = [anchor['href'] for anchor in td_list[-3].find_all('a')]
 
     if debug:
         print(td_list)
         print('작성자 : ' + writer)
         print('제목 : ' + title)
-        # print(files)
+        print('날짜 : ' + date)
         print('파일 : ' + str(files))
-    return {'writer': writer, 'title': title, 'files': files}
+    if isWrite:
+        f.write('작성자 : ' + writer + '\n')
+        f.write('제목 : ' + title + '\n')
+        f.write('날짜 : ' + date + '\n')
+        f.write('파일 : ' + str(files) + '\n')
+    return {'writer': writer, 'title': title, 'files': files, 'date': date}
 
 
-# f(base_url, base_no, jsessionid, True)
+# read(base_url, base_no, jsessionid, True)
 i = 0
-while base_no - i >= 0:
+while base_no + i >= 0:
     print('#' * 10 + str(base_no - i))
-    info = f(base_url, base_no - i, jsessionid)
+    info = read(base_url, base_no - i, jsessionid, True)
+    print('날짜 : ' + info['date'])
     if info['writer'] == '김성수':
         print('작성자 : ' + info['writer'])
         print('제목 : ' + info['title'])
         print('파일 : ' + str(info['files']))
     i += 1
+f.close()
